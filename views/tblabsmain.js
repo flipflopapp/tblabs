6936b6b247b93ae9d98ad34a481d08b84b4441f2
flipflopapp <navalnovel@gmail.com>
2013-03-31 13:30:27 +0530

    Auto-zoom based on radius implemented

--- views/tblabsmain.js
+++ views/tblabsmain.js
@@ -2,7 +2,7 @@
 
     // Config for the App
     var AppCfg = { 
-        zoom: 13,
+        zoom: 15,
         current: {
             address: "",
             latLng: new google.maps.LatLng(28.6667, 77.2169)
@@ -95,16 +95,20 @@
                 me.gMap.delegateEvents();
             }
             me.radius = $("#container").find("#radius");
-            me.radius.bind('change', this.changeRadius);
+            me.radius.bind('change', me.doRadiusChange);
+        },
+
+        doRadiusChange: function() {
+            var value = parseInt ( event.srcElement.value );
+            SearchCfg.radius = value;
         },
 
         doSearch: function() {
-            tblabcollection.fetch();
-        },
-
-        changeRadius: function(event) {
-            var value = parseInt ( event.srcElement.value );
-            SearchCfg.radius = value;
+            var me = this;
+            var address = { address: $(".search-query")[0].value };
+            if ( address !== AppCfg.searchtxt ) {
+                me.gMap.setMapCenter(address); 
+            }
         }
     });
 
@@ -113,33 +117,20 @@
 
         render: function() {
             var me = this;
-
             //this.collection.each( function(tbcenter) {
             //    console.log ( tbcenter );
             //});
-
             return this;
         },
 
         initialize: function() {
             var me = this;
-
-            // INITIALIZE GOOGLE MAP
-            me.$el.gmap3({
-              map:{
-                options:{
-                  center:SearchCfg.center,
-                  zoom: AppCfg.zoom
-                }
-              }
-            });
-
+            me.geocoder = new google.maps.Geocoder();
             me.doGeoLocation();
-
-            this.collection.bind("change", function() { console.log ( 'change is not implemented' ) } ); 
-            this.collection.bind("reset", function() { console.log ( 'reset is not implemented' ) } );   
+            this.collection.bind("reset", this.resetTBCenters, this); 
             this.collection.bind("add", this.addTBCenters, this); 
-            this.collection.bind("remove", this.removeTBCenters, this); 
+            this.collection.bind("change", function() { console.log ( 'changeTBCenters is not implemented' ) } ); 
+            this.collection.bind("remove", function() { console.log ( 'removeTBCenters is not implemented' ) } ); 
         },
 
         addTBCenters: function() {
@@ -158,11 +149,13 @@
             });
         },
         
-        removeTBCenters: function() {
-            console.log('remove');
-            console.log(arguments[0].attributes);
+        resetTBCenters: function() {
+            var me = this;
+            var clear = {name:"marker"};
+            clear.tag = "tbcenters";
+            me.$el.gmap3({clear:clear});
         },
-
+            
         doBreakupAddress: function(ac, types) {
             var me = this;
             var str = "";
@@ -181,36 +174,59 @@
             } )
             return (str);
         },
+
+        setMapCenter: function(addr_or_coord) {
+            var me = this;
+            me.geocoder.geocode(addr_or_coord, function(results, status) {
+               if (status == google.maps.GeocoderStatus.OK && results.length) {
+                   var ac = results[0].address_components;
+                   SearchCfg.local = me.doBreakupAddress(ac, AppCfg.localGMapTypes);
+                   SearchCfg.city = me.doBreakupAddress(ac, AppCfg.cityGMapType);
+                   SearchCfg.state = me.doBreakupAddress(ac, AppCfg.stateGMapType);
+                   SearchCfg.country = me.doBreakupAddress(ac, AppCfg.countryGMapType);
+                   SearchCfg.pincode = me.doBreakupAddress(ac, AppCfg.pinGMapType);
+                   AppCfg.searchtxt = SearchCfg.local;
+                   AppCfg.current.latLng = results[0].geometry.location;
+
+                   me.collection.reset();
+                   me.collection.fetch();
+                   $(".search-query")[0].setAttribute("value", AppCfg.searchtxt);
+
+                   // clear previous marker if set
+                   var clear = {name:"marker"};
+                   clear.tag = "mypog";
+
+                   me.$el.gmap3({
+                     map:{
+                       options:{
+                         center:AppCfg.current.latLng,
+                         zoom: me.getZoomFromRadius(),
+                         zoomControl: true,
+                         panControl: true,
+                         scaleControl: true
+                       }
+                     },
+                     clear:clear,
+                     marker:{
+                       latLng:AppCfg.current.latLng,
+                       options:{
+                         icon:new google.maps.MarkerImage("http://maps.gstatic.com/mapfiles/icon_greenA.png")
+                       },
+                       tag: "mypog"
+                     }
+                   });
+               }
+            });
+        },
  
         doGeoLocation: function() {
             var me = this;
             if (navigator.geolocation) {
               navigator.geolocation.getCurrentPosition(
                 function( position ) {
-                  AppCfg.current.latLng = new google.maps.LatLng(position.coords.latitude, 
-                                                         position.coords.longitude);
-                  me.$el.gmap3("get").setCenter(AppCfg.current.latLng);
-                  me.$el.gmap3({
-                    marker:{
-                      latLng:AppCfg.current.latLng,
-                      options:{
-                        icon:new google.maps.MarkerImage("http://maps.gstatic.com/mapfiles/icon_greenA.png")
-                      }
-                    }
-                  });
-                  var geocoder = new google.maps.Geocoder();
-                  geocoder.geocode(AppCfg.current, function(results, status) {
-                        if (status == google.maps.GeocoderStatus.OK && results.length) {
-                            var ac = results[0].address_components;
-                            SearchCfg.local = me.doBreakupAddress(ac, AppCfg.localGMapTypes);
-                            SearchCfg.city = me.doBreakupAddress(ac, AppCfg.cityGMapType);
-                            SearchCfg.state = me.doBreakupAddress(ac, AppCfg.stateGMapType);
-                            SearchCfg.country = me.doBreakupAddress(ac, AppCfg.countryGMapType);
-                            SearchCfg.pincode = me.doBreakupAddress(ac, AppCfg.pinGMapType);
-                            AppCfg.searchtxt = SearchCfg.local;
-                            $(".search-query")[0].setAttribute("value", AppCfg.searchtxt);
-                        }
-                  });
+                  var current = { latLng: new google.maps.LatLng(position.coords.latitude, 
+                                                         position.coords.longitude) };
+                  me.setMapCenter (current);
                 },
                 function ( error ) {
                   console.log ("Something went wrong: ", error);
@@ -222,6 +238,25 @@
                 }
               );
             }
+        },
+
+        getZoomFromRadius: function() {
+            var radius = SearchCfg.radius
+              , zoom;
+            if ( radius == 3 ) {
+                zoom = 14; 
+            } else if ( radius <=5 ) {
+                zoom = 13;
+            } else if ( radius < 10 ) {
+                zoom = 12;
+            } else if ( radius <= 20 ) {
+                zoom = 11;
+            } else if ( radius <= 40 ) {
+                zoom = 10;
+            } else {
+                zoom = 9;
+            }
+            return zoom;
         }
     });
 
